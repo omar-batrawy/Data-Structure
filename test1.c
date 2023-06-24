447abec37752a99f19b39049a19b985d637faef1
ombat <110371340+ombat@users.noreply.github.com>
2023-06-24 14:35:31 +0300

    initialize

---
+++ test1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    int a = 10;
+    int b;
+    scanf("%d", &b);
+    printf("Hello World!\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    getch();
+}
